b011632b37551f790bb7140c8e88932e3c4e106a
kevinchen850913 <kevinchen850913@gmail.com>
2022-09-20 14:00:26 +0800

    718. Maximum Length of Repeated Subarray

---
+++ 718. Maximum Length of Repeated Subarray.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int FindLength(int[] nums1, int[] nums2)
+    {
+        if (nums1 == null || nums2 == null)
+            return 0;
+        int max = 0;
+        int[,] dp = new int[nums1.Length + 1, nums2.Length + 1];
+        for (int i = 1; i <= nums1.Length; i++)
+        {
+            for (int j = 1; j <= nums2.Length; j++)
+            {
+                if (nums1[i - 1] == nums2[j - 1])
+                {
+                    dp[i, j] = 1 + dp[i - 1, j - 1];
+                    max = Math.Max(max, dp[i, j]);
+                }
+            }
+        }
+        return max;
+    }
+}
