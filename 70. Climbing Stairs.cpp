e89153d4ec752d11482f1506d86310693fde4cee
kevinchen850913 <kevinchen850913@gmail.com>
2022-12-12 11:48:00 +0800

    70. Climbing Stairs

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n < 3)
+        {
+            return n;
+        }
+        int nums[n];
+        nums[0] = 1;
+        nums[1] = 2;
+        for (int i = 2; i < n; i++)
+        {
+            nums[i] = nums[i - 1] + nums[i - 2];
+        }
+        return nums[n - 1];
+    }
+};
